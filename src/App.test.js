8c7167dddcf01aade8a2ef7af6c63d0bae1c190b
Jordan <jordanleelong5@gmail.com>
2020-11-24 10:05:46 -0800

    added two tests to make sure app renders with no errors, and check to make sure Count renders without an error

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {shallow} from 'enzyme'; 
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+test('app renders with no error', () => {
+    const wrapper = shallow(<App />); 
+    const component = wrapper.find('.app-container'); 
+    expect(component.exists()).toBe(true);
 });
+
+test('renders the Count component', () => {
+  const wrapper = shallow(<App />); 
+  const count = wrapper.find('Count'); 
+  expect(count.exists()).toBe(true);
+})
